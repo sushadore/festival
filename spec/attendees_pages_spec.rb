571b65f38ab83b92e25a663248128c820066bb18
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-18 14:52:35 -0700

    add integration specs

--- spec/attendees_pages_spec.rb
+++ spec/attendees_pages_spec.rb
@@ -1,1 +1,20 @@
 require "spec_helper"
+
+describe('attendees can create an account', {:type => :feature}) do
+  it('allows festival attendees to signup for an account') do
+    visit('/')
+    fill_in("name", :with => "sowmya")
+    fill_in("username", :with => "sowmya1")
+    fill_in("password", :with => "sowmya2")
+    click_button('Add User')
+    expect(page).to have_content("Hello Sowmya!")
+  end
+end
+
+describe('attendee logs in to the account after signing up', {:type => :feature}) do
+  it('allows an attendee to add an artist') do
+    @attendee = Attendee.create(name: "Steven", username: "steventest", password: "123456t", id: 1)
+    visit('/attendee/1')
+    expect(page).to have_content("There are no artists to add right now. Check back later!")
+  end
+end
